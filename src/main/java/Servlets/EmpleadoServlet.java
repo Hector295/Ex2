e1eb0d88f7e227ab19e3f60d94ce9ee09a608929
Hector Ventura <hector.ventura@pucp.edu.pe>
2020-12-21 09:36:15 -0500

    Hector

--- src/main/java/Servlets/EmpleadoServlet.java
+++ src/main/java/Servlets/EmpleadoServlet.java
@@ -1,5 +1,6 @@
 package Servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +15,19 @@ public class EmpleadoServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
 
+        if (request.getSession().getAttribute("empleado") == null) {
+            response.sendRedirect(request.getContextPath());
+        } else {
+            String accion = request.getParameter("accion") == null ? "lista" : request.getParameter("accion");
+
+            RequestDispatcher view;
+            switch (accion){
+                case "":
+                    break;
+            }
+
+        }
     }
 }
